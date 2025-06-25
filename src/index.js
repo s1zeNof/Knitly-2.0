307182034fed37e68fe5c2555a7ce086e5379e2a
s1zeNof <branovskyi@gmail.com>
2025-06-26 00:19:28 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Add this to the top of src/index.js to support environments without crypto.randomUUID
+if (typeof crypto.randomUUID !== 'function') {
+  crypto.randomUUID = function randomUUID() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  };
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
