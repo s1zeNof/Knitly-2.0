7676dd62e7383a592375497726feda8a419c0c3b
s1zeNof <branovskyi@gmail.com>
2025-07-04 17:09:49 +0300

    Updated header styles

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -11,7 +11,7 @@ const MenuIcon = () => <svg width="24" height="24" viewBox="0 0 24 24" fill="non
 const BellIcon = () => <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>;
 const MessagesIcon = () => <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>;
 
-const Header = ({ showTagPageMenu, onTagPageMenuClick }) => {
+const Header = () => {
     const { user, totalUnreadMessages, unreadNotificationsCount } = useUserContext();
     const navigate = useNavigate();
     const location = useLocation();
@@ -75,7 +75,7 @@ const Header = ({ showTagPageMenu, onTagPageMenuClick }) => {
             </div>
             
             {!user ? (
-                <div className="auth-buttons">
+                <div className="right-side-header auth-buttons">
                     <button className="btn-login button" onClick={() => navigate('/login')}>Log In</button>
                     <button className="btn-signup button" onClick={() => navigate('/register')}>Sign Up</button>
                 </div>
