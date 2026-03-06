e110611cfc0e86edcef26cef2a76a936ba02e357
s1zeNof <branovskyi@gmail.com>
2026-03-06 23:30:13 +0200

    fix(build): removed unused useCallback in EmailVerificationBanner to fix CI error

--- src/components/common/EmailVerificationBanner.jsx
+++ src/components/common/EmailVerificationBanner.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { sendEmailVerification } from 'firebase/auth';
 import { auth } from '../../services/firebase';
 import { useUserContext } from '../../contexts/UserContext';
